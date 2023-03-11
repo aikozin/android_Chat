fe2373efa3ac2ae32197613188985be9cfe8ae0c
3b_Manych <vovcka_lucachev@mail.ru>
2023-03-11 10:37:48 +0300

    Работаем над получением настроек

--- app/src/main/java/ru/example/chat/MainActivity.java
+++ app/src/main/java/ru/example/chat/MainActivity.java
@@ -2,13 +2,44 @@ package ru.example.chat;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    EditText password;
+    EditText username;
+    Button enterbtn;
+    SharedPreferences mSharedPref;
+    final String USERNAME = "Логин";
+    final String PASSWORD = "Пароль";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        password = findViewById(R.id.password);
+        username = (EditText) findViewById(R.id.username);
+        enterbtn = (Button) findViewById(R.id.enterbtn);
+
+        mSharedPref = getPreferences (MODE_PRIVATE);
+        boolean isAuth = mSharedPref.getBoolean("isAuth", false);
+        if (isAuth){
+
+        }
+        else{
+
+        }
     }
+    public void save(View view){
+        SharedPreferences.Editor mEditor = moveSharedPref.edit();
+        mEditor.putString(USERNAME, username.getText().toString());
+        mEditor.putString(PASSWORD, password.getText().toString());
+        Toast.makeText(this, "Данные сохранены", Toast.LENGTH_SHORT).show();
+    }
+
 }
