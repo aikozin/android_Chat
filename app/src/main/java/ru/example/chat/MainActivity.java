0d11308ed5f150906a34ecaa194d7d54f3022094
Nikolay Kot <snowjeannedarc@mail.ru>
2023-03-19 12:44:26 +0300

    DEV003 - TODO: SPEC004 Косметическое "обновление" + отладка интерфейса

--- app/src/main/java/ru/example/chat/MainActivity.java
+++ app/src/main/java/ru/example/chat/MainActivity.java
@@ -9,6 +9,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_messages); // Для отладки изменил c: activity_main
     }
 }
